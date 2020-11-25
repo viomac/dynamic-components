5df191720092e43f387b6f2b7dc88d1fc709a022
U-SOF\vime <vime@PCVIME.sofico.be>
2020-11-25 17:45:17 -0600

    Add dialog component
    We add:
    
    - Dialog effect
    - Callbacks to the component
    
    We make sure that the click event on the dialog itself is not propagated to the parent elements.
    
    Otherwise, the onOverlayClicked callback would always fire, even if we hit the dialog itself and not the overlay (the overlay is the gray area around the dialog). We do so in the onDialogClicked method.
    
    Also, the component has to know which type of component it will has to create later. For that, we are adding the property childComponentType.

--- src/app/dialog-system/dialog-system.module.ts
+++ src/app/dialog-system/dialog-system.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { DialogComponent } from './dialog.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [DialogComponent],
   imports: [
     CommonModule
   ]
---
+++ src/app/dialog-system/dialog.component.ts
@@ -0,0 +1,28 @@
+import {Component, AfterViewInit, OnDestroy, Type} from '@angular/core';
+
+@Component({
+  selector: 'app-dialog',
+  template: `
+    <div class="overlay" (click)="onOverlayClicked($event)">
+        <div class="overlay" (click)="onDialogClicked($event)">
+        </div>
+    </div>
+  `,
+  styleUrls: ['./dialog.component.css']
+})
+export class DialogComponent implements AfterViewInit, OnDestroy {
+  childComponentType: Type<any>;
+  constructor() { }
+
+  ngAfterViewInit(): void {
+  }
+
+  ngOnDestroy(): void {}
+
+  onOverlayClicked(evt: MouseEvent): void {}
+
+  onDialogClicked(evt: MouseEvent): void {
+    evt.stopPropagation()
+  }
+
+}
