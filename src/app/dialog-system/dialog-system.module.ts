e98ed4b9c6e815c537d647c289bad5ca8eb5e712
U-SOF\vime <vime@PCVIME.sofico.be>
2020-11-26 11:51:35 -0600

    Move the dialog component to the components directory

--- src/app/dialog-system/dialog-system.module.ts
+++ src/app/dialog-system/dialog-system.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { DialogComponent } from './dialog.component';
+import { DialogComponent } from './components/dialog.component';
 
 
 
