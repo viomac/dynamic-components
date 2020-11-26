973f22dc72cdd50479704422d0ae50de98f6e513
U-SOF\vime <vime@PCVIME.sofico.be>
2020-11-26 15:27:06 -0600

    Append the dialog to the HTML-body
    Dinamically instantiating the dialog component using its factory

---
+++ src/app/dialog/services/dialog.service.ts
@@ -0,0 +1,44 @@
+import {ApplicationRef, ComponentFactoryResolver, ComponentRef, EmbeddedViewRef, Injectable, Injector} from '@angular/core';
+import {DialogComponent} from '../components/dialog.component';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DialogService {
+  dialogComponentRef: ComponentRef<DialogComponent>;
+
+  constructor(
+    private componentFactoryResolver: ComponentFactoryResolver,
+    private appRef: ApplicationRef,
+    private injector: Injector
+  ) { }
+
+  appendDialogComponentToBody(): void {
+    // To get the factory of our DialogComponent we can use
+    // the ComponentFactoryResolver provided by angular.
+    // This service is using the type of the component to look up the factory.
+    const componentFactory = this.componentFactoryResolver
+      .resolveComponentFactory(DialogComponent);
+
+    // Once we have the factory, we can use it to create
+    // an instance of our DialogComponent.
+    // We are passing in the injector we requested
+    // in the constructor. This enables the dynamic component
+    // to make use of dependency injection itself.
+    const componentRef = componentFactory.create(this.injector);
+
+    // Afterward, we need to attach the new component to
+    // the angular component tree (which is separate from the DOM).
+    // We do so by using the ApplicationRef we requested.
+    this.appRef.attachView(componentRef.hostView);
+
+    // We get the root DOM-element of our DialogComponent
+    // and attach it to the HTML-body. Also, we assign the
+    // componentRef to our property.
+    const domElem = (componentRef.hostView as EmbeddedViewRef<any>)
+      .rootNodes[0] as HTMLElement;
+
+    document.body.appendChild(domElem);
+    this.dialogComponentRef = componentRef;
+  }
+}
